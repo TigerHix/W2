9663f241570363b8f1d7e14be0ab9c1b9baf69d4
tan279 <tan279@purdue.edu>
2020-04-25 19:48:17 -0400

    First sectionize rects into row sections

--- src/main/scala/w2d/Wireframe2DOM.scala
+++ src/main/scala/w2d/Wireframe2DOM.scala
@@ -4,14 +4,30 @@ object Wireframe2DOM extends App {
   Examples.Basic1()
 
   def synthesize(rects: Seq[Rect], width: Int, height: Int): Container = {
-    new WireframeVisualizer(rects, width, height).main(Array())
-    val rectSortedByRow = rects.sortWith(_.origin.y < _.origin.y)
-    val rowSet = (rectSortedByRow map {case rect => rect.origin.y + rect.size.y}).toSet
-    val rectYRange = rectSortedByRow map {case rect => rect.origin.y to rect.origin.y + rect.size.y}
-    // now we want to put rects that are potentially in the same container together
-    
-    
-
+    def rectLowerY(rect: Rect):Int = {
+      rect.origin.y + rect.size.y
+    }
+    //new WireframeVisualizer(rects, width, height).main(Array())
+    val rectSortedByRow = rects.sortWith(rectLowerY(_) < rectLowerY(_))
+    val rectNumber = rects.length
+    // sort the rects' lower y and obtain all distinct values
+    val rowSorted = (rectSortedByRow map {case rect => rectLowerY(rect)}).toSet.toSeq.sortWith(_ < _)
+    val rectYRange = rectSortedByRow map {case rect => rect.origin.y until rectLowerY(rect)}
+    val clearRow = rowSorted filter {case row => (rectYRange filter {case range => range.contains(row)}).length == 0}
+    var addedUntil = 0
+    val firstCutByRow : Map[Int, Seq[Rect]] = (clearRow map {case row => 
+      row -> {
+        var rectSeq = Seq[Rect]()
+        var counter = addedUntil
+        while (counter < rectNumber && rectLowerY(rectSortedByRow(counter)) <= row) {
+          rectSeq = rectSeq :+ rectSortedByRow(counter)
+          counter += 1
+        }
+        addedUntil = counter
+        rectSeq
+      }
+    }).toMap
+    println(firstCutByRow)
 
     null
   }
